20d73166df41413cb7b9524079a96b57a177f795
Frank Buono <fbuonojr@fordham.edu>
2020-12-09 13:26:54 -0500

    added data for characters

--- server.js
+++ server.js
@@ -9,3 +9,27 @@ var PORT = 3000;
 //sets up app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var characters = [
+    {
+        routeName: "yoda",
+        name: "Yoda",
+        role: "Jedi Master",
+        age: 900,
+        forcePoints: 2000
+    },
+    {
+        routeName: "darthmaul",
+        name: "Darth Maul",
+        role: "Sith Lord",
+        age: 200,
+        forcePoints: 1200
+    },
+    {
+        routeName: "obiwankenobi",
+        name: "Obi Wan Kenobi",
+        role: "Jedi Master",
+        age: 55,
+        forcePoints: 1350
+    }
+];
