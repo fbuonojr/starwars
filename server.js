c465a7d4d974b56f325fea04d36515c3e24e476d
Frank Buono <fbuonojr@fordham.edu>
2020-12-09 13:26:28 -0500

    set up express app

--- server.js
+++ server.js
@@ -2,3 +2,10 @@
 var express = require("express");
 var path = require("path");
 
+//express app
+var app = express();
+var PORT = 3000;
+
+//sets up app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
